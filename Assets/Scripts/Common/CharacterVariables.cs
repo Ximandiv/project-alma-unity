91b0fce230f53804272a172f8f20469a21908960
Ximandiv <ximandiv@gmail.com>
2024-12-18 10:15:42 -0500

    Adds CharacterVariables script

---
+++ Assets/Scripts/Common/CharacterVariables.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+namespace Scripts.Common
+{
+    public class CharacterVariables : MonoBehaviour
+    {
+        #region Public Methods
+
+        #region Set Methods
+
+        public void SetCurrentHitpoints(int amount)
+        {
+            if(amount < minHitPoints)
+                amount = minHitPoints;
+            else if(amount > maxHitPoints)
+                amount = maxHitPoints;
+
+            currentHitpoints = amount;
+        }
+
+        public void SetCurrentSpeed(float amount)
+        {
+            if (amount < minSpeed)
+                amount = minSpeed;
+            else if (amount > maxSpeed)
+                amount = maxSpeed;
+
+            currentSpeed = amount;
+        }
+
+        public void SetMovingStatus() => isMoving = !isMoving;
+
+        public void PauseCharacter() => isGamePaused = !isGamePaused;
+
+        #endregion
+
+        #region Get Methods
+
+        public int GetCurrentHitpoints() => currentHitpoints;
+        public float GetCurrentSpeed() => currentSpeed;
+        public bool IsMoving() => isMoving;
+
+        #endregion
+
+        #endregion
+
+        #region Private Variables
+
+        [Header("Status")]
+
+        [SerializeField] private bool isGamePaused = false;
+        [SerializeField] private bool isMoving = false;
+
+        [Header("Current Variables")]
+
+        [SerializeField] private int currentHitpoints = 1;
+        [SerializeField] private float currentSpeed = 5f;
+
+        [Header("Range of Variables")]
+
+        [SerializeField] private int minHitPoints = 1;
+        [SerializeField] private int maxHitPoints = 2;
+
+        [SerializeField] private float minSpeed = 1f;
+        [SerializeField] private float maxSpeed = 10f;
+
+        #endregion
+    }
+}
