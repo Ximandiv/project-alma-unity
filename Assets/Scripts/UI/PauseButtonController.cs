6529390360b7c4d884da49ad8cd363aa7cd2e861
Juana Rodas <jrodas033@gmail.com>
2024-12-22 20:32:55 -0500

    Add PauseButtonController script

---
+++ Assets/Scripts/UI/PauseButtonController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Scripts.Scriptables;
+
+namespace Scripts.UI
+{
+    public class PauseButtonController : MonoBehaviour
+    {
+        #region Private Variables
+
+        [SerializeField] private GameStatus gameStatus;
+        [SerializeField] private GameObject pausePanel;
+        [SerializeField] private GameObject menuPanel;
+        [SerializeField] private KeyCode keyToMenu = KeyCode.Escape;
+
+        #endregion
+        
+        #region Unity API Methods
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(keyToMenu))
+            {
+                ActivateButton();
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ActivateButton()
+        {  
+            gameStatus.Pause();
+            menuPanel.SetActive(!menuPanel.activeSelf);
+            pausePanel.SetActive(!pausePanel.activeSelf);
+        }
+        
+        #endregion
+    }
+}
+
